70d3087e76bec7ed9031f54a8fe3049faf27ac4e
Jukinner <jukinner@gmail.com>
2018-06-12 22:19:08 -0600

    Added final few tests to round out

--- calculator_test/spec.js
+++ calculator_test/spec.js
@@ -21,40 +21,99 @@
 //   });
 // });
 
+// describe('Protractor Demo App', function() {
+//   var firstNumber = element(by.model('first'));
+//   var secondNumber = element(by.model('second'));
+//   var goButton = element(by.id('gobutton'));
+//   var latestResult = element(by.binding('latest'));
+
+//   beforeEach(function() {
+//     browser.get('http://juliemr.github.io/protractor-demo/');
+//   });
+
+//   it('should have a title', function() {
+//     expect(browser.getTitle()).toEqual('Super Calculator');
+//   });
+
+//   it('should add one and two', function() {
+//     firstNumber.sendKeys(1);
+//     secondNumber.sendKeys(2);
+
+//     goButton.click();
+
+//     expect(latestResult.getText()).toEqual('3');
+//   });
+
+//   it('should add four and six', function() {
+//     firstNumber.sendKeys(4);
+//     secondNumber.sendKeys(6);
+
+//     goButton.click();
+
+//     expect(latestResult.getText()).toEqual('10');
+//   });
+
+//   it('should read the value from an input', function() {
+//     firstNumber.sendKeys(1);
+//     expect(firstNumber.getAttribute('value')).toEqual('1');
+//   });
+// });
+
+
+// spec.js
+// describe('Protractor Demo App', function() {
+//   var firstNumber = element(by.model('first'));
+//   var secondNumber = element(by.model('second'));
+//   var goButton = element(by.id('gobutton'));
+//   var latestResult = element(by.binding('latest'));
+//   var history = element.all(by.repeater('result in memory'));
+
+//   function add(a, b) {
+//     firstNumber.sendKeys(a);
+//     secondNumber.sendKeys(b);
+//     goButton.click();
+//   }
+
+//   beforeEach(function() {
+//     browser.get('http://juliemr.github.io/protractor-demo/');
+//   });
+
+//   it('should have a history', function() {
+//     add(1, 2);
+//     add(3, 4);
+
+//     expect(history.count()).toEqual(2);
+
+//     add(5, 6);
+
+//     expect(history.count()).toEqual(3);
+//   });
+// });
+
+
+
 describe('Protractor Demo App', function() {
   var firstNumber = element(by.model('first'));
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
 
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
 
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
-
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function() {
-    firstNumber.sendKeys(4);
-    secondNumber.sendKeys(6);
-
-    goButton.click();
-    
-    expect(latestResult.getText()).toEqual('10');
-  });
-
-  it('should read the value from an input', function() {
-    firstNumber.sendKeys(1);
-    expect(firstNumber.getAttribute('value')).toEqual('1');
+    expect(history.last().getText()).toContain('1 + 2');
+    expect(history.first().getText()).toContain('3 + 4'); 
   });
 });
